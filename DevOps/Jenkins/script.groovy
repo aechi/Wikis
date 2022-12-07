83f51494f68d6e9a34f87de05b682a67ea9fef1f
aechi <86666168+aechi@users.noreply.github.com>
2022-12-07 17:04:16 +0100

    Update script.groovy

--- DevOps/Jenkins/script.groovy
+++ DevOps/Jenkins/script.groovy
@@ -1,25 +1,7 @@
-#!groovy
-@Library('shared') _
-
 
 def tests() {
     return ["Test01", "Test2", "Test4"]
 }
-// Call shared libaray for common params
-def paramList = jobParams.listParams ([
-    "var1": "value",
-    "var2": "value2"
-])
-// Define repo specific params
-def addtionalParams = [
-    booleanParam(defaultValue: false, name: 'SOMETHING', description: 'description?'),
-    booleanParam(defaultValue: false, name: 'SOMETHING_ELSE', description: 'description?'),
-]
-// Set Jenkins job properties, combining both
-properties([
-    buildDiscarder(logRotator(numToKeepStr: '20')),
-    parameters(paramList + addtionalParams)
-])
 
 def buildApp() {
     echo 'building the application...'
